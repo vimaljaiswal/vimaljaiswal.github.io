efd7da3acf8cffaa3cf6ee1f90c0ac8afaeef9a1
vimaljaiswal <Vimal.Jaiswal@exponential.com>
2017-02-03 15:05:36 +0530

    v4
    v4

---
+++ testHack/VDX-Infinity-game/v1/files/main.js
@@ -0,0 +1,12 @@
+(function($){
+
+    $(function(){
+
+        /* Sticky Tab */
+
+        /* Fifty-50 Slider */
+        $(".fifty50Banner").fifty50Slider();
+
+
+    });/*Document Ready ends here*/
+}(jQuery));
---
+++ testHack/VDX-Infinity-game/v1/files/renderer.js
@@ -0,0 +1,41 @@
+    function timerCalled(timer) {
+        // console.log(timer);
+        if (timer >= 485) {
+           // alert("open VPM");
+            //  console.log("timer called ....  video ")
+        }
+        else {
+           // alert("open mainunit");
+            // console.log("timer called ....   img")
+        }
+    }
+
+
+
+    function chkRollover(event) {
+        if (event.id == "videoTab") {
+            // console.log("tchkRollover .... video  ")
+        }
+        else {
+           // console.log("tchkRollover .... image  " + event.id)
+        }
+    }
+
+    function chkRollout(event) {
+        if (event.id == "videoTab") {
+            // console.log("tchkRollout .... video  ")
+        }
+        else {
+             //console.log("tchkRollout .... image  " + event.id)
+        }
+    }
+
+    function chkClick(event) {
+        console.log("tchkclick ....  " + event.id)
+        if (event.id == "videoTab") {
+             console.log("tchkclick .... video  ")
+        }
+        else if (event.id == "imageTab")  {
+             console.log("tchkclick .... image  " + event.id)
+        }
+    }
